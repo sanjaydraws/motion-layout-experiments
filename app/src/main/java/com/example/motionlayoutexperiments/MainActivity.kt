d75d9b0750204dd3f6e230c7cfa8e41f66f2c243
sanjaydraws <sprajapat8331@gmail.com>
2021-09-02 00:56:42 +0530

    constrain sets

--- app/src/main/java/com/example/motionlayoutexperiments/MainActivity.kt
+++ app/src/main/java/com/example/motionlayoutexperiments/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.motionlayoutexperiments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.motionlayoutexperiments.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    var binding:ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding?.apply {
+            setContentView(binding?.root)
+        }
+
+
     }
 }
